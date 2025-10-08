ddcfd6c8285e03dcb311fd7c86c6f429dc7f6245
Backend Developer <hseyn.mmmdzad@bk.ru>
2025-10-08 14:08:16 +0400

    Add files via upload

---
+++ AvtomobilQalereyasi.java
@@ -0,0 +1,22 @@
+package arraylist;
+
+import java.util.LinkedList;
+
+public class AvtomobilQalereyasi {
+
+    public static void main(String[] args) {
+
+
+        LinkedList<String> masinlar = new LinkedList<>();
+
+        masinlar.add("Mercedes");
+        masinlar.add("BMW");
+        masinlar.add("Audi");
+
+
+        System.out.println("Galereyadaki Masinlarin Siyahisi : " + masinlar);
+
+    }
+
+
+}
